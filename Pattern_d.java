3a6e1e568fec15c62562f698c64480276cd8d93b
Ambika Kumari <145006866+Ambika010@users.noreply.github.com>
2023-10-31 14:45:54 +0530

    Add files via upload

---
+++ Pattern_d.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Pattern_d {
+    public static void nTriangle(int n) {
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print(i+" ");
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        nTriangle(n);
+    }
+}
