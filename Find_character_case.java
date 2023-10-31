a7fd407db8fdc6e838617f499bc6580a42cf47b3
Ambika Kumari <145006866+Ambika010@users.noreply.github.com>
2023-10-31 12:14:50 +0530

    Add files via upload

---
+++ Find_character_case.java
@@ -0,0 +1,18 @@
+//Step 1.1 Question 1
+import java.util.*;
+class Find_character_case{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        char ch = sc.next().charAt(0);
+        int c = (int)ch; //typecasting character into ASCII values
+        if(c>=65 && c<=90){ 
+            System.out.println("1");
+        }
+        else if(c>=97 && c<=122){
+            System.out.println("0");
+        }
+        else{
+            System.out.println("-1");
+        }
+    }
+}
