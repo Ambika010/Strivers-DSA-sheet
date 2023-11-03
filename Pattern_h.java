5165e7b9d003801407af0bf484b7145c5c375fb0
Ambika Kumari <145006866+Ambika010@users.noreply.github.com>
2023-11-03 21:07:10 +0530

    Add files via upload

---
+++ Pattern_h.java
@@ -0,0 +1,22 @@
+public class Pattern_h {
+    public static void numberCrown(int n) {
+        for(int i=1;i<=n;i++){
+            int v=i;
+            for(int j=1;j<=i;j++){
+                System.out.print(j);
+            }
+            int space=2*(n-i);
+            for(int s=1;s<=space;s++){
+                System.out.print(" ");
+            }
+            for(int j=1;j<=i;j++){
+                System.out.print(v);
+                v=v-1;
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+        numberCrown(5);
+    }
+}
