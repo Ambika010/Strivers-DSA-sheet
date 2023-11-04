f292f24f957bb36ebe9f376c72beb7f6c0f534d4
Ambika Kumari <145006866+Ambika010@users.noreply.github.com>
2023-11-04 21:47:37 +0530

    Add files via upload

---
+++ Pattern_m.java
@@ -0,0 +1,20 @@
+public class Pattern_m {
+    public static void alphaHill(int n){
+        for(int i=1;i<=n;i++){
+            int space=n-i;
+            for(int s=1;s<=space;s++){
+                System.out.print(" ");
+            }
+            for(int j=1;j<=i;j++){
+                System.out.print((char)('A'+j-1));
+            }
+            for(int j=i-1;j>=1;j--){
+                System.out.print((char)('A'+j-1));
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+        alphaHill(5);
+    }
+}
