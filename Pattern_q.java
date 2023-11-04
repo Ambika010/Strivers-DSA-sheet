fd8951f86d71da063e3619b5c7626293346db205
Ambika Kumari <145006866+Ambika010@users.noreply.github.com>
2023-11-05 01:21:11 +0530

    Add files via upload

---
+++ Pattern_q.java
@@ -0,0 +1,35 @@
+public class Pattern_q {
+    public static void symmetry(int n) {
+        //upper part till n
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            int space=2*(n-i);
+            for(int s=1;s<=space;s++){
+                System.out.print(" ");
+            }
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        //lower part after n
+        for(int i=n-1;i>=1;i--){
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            int space=2*(n-i);
+            for(int s=1;s<=space;s++){
+                System.out.print(" ");
+            }
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+        symmetry(8);
+    }
+}
