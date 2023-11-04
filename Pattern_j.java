eb8e77da6514038644c9646afe4334124835353a
Ambika Kumari <145006866+Ambika010@users.noreply.github.com>
2023-11-04 21:46:28 +0530

    Add files via upload

---
+++ Pattern_j.java
@@ -0,0 +1,13 @@
+public class Pattern_j{
+    public static void nLetterTriangle(int n){
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print((char)('A'+j-1)+" ");
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+        nLetterTriangle(6);
+    }
+}
