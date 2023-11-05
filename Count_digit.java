5e4c511cb6261ecdac66ac8e7c10eafea2305fdc
Ambika Kumari <145006866+Ambika010@users.noreply.github.com>
2023-11-05 22:15:16 +0530

    Add files via upload

---
+++ Count_digit.java
@@ -0,0 +1,24 @@
+//count only that number of digits which divides the number n completely 
+import java.util.*;
+public class Count_digit {
+    public static int countDigits(int n){
+        int count=0;
+        int x=n;
+        while(x!=0){
+            int r=x%10;
+            x=x/10;
+            try{
+                if(n%r==0){
+                count++;
+                }
+            }
+            catch(Exception e){};
+        }
+        return count;
+    }
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int num=sc.nextInt();
+        System.out.println(countDigits(num));
+    }
+}
