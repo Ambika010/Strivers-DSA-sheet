110197ab66e3645405458b394204d7630bf8fc47
Ambika Kumari <145006866+Ambika010@users.noreply.github.com>
2023-11-08 23:05:25 +0530

    Add files via upload
    Armstrong Numbers are the numbers having the sum of digits raised to power no. of digits is equal to a given number.
    example : 
    153 is an Armstrong Number
    Explanation: 1^3 + 5^3 + 3^3 = 153

---
+++ Armstrong.java
@@ -0,0 +1,29 @@
+import java.util.*;
+public class Armstrong {
+    public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int x=n;
+		int sum=0;
+		int count=0;
+		// this while loop counts the number of digits
+		while(x!=0){
+			x=x/10;
+			count++;
+		}
+        x=n;
+		// this loop calculates the sum of digits raised to the power of no. of digits i.e
+		// if n=340 then this while loop calculates 3^3+4^3+0^3
+		while(x!=0){
+			int rem=x%10;
+			sum=sum+(int)Math.pow(rem, count);
+            x=x/10;
+		}
+		if(n==sum){
+			System.out.println("true");
+		} 
+        else{
+			System.out.println("false");
+		}
+	}
+}
