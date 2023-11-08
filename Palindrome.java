7b22fe0105558b73f37b78ffa4bebd5a8684f0fb
Ambika Kumari <145006866+Ambika010@users.noreply.github.com>
2023-11-08 22:46:24 +0530

    Add files via upload
    A number is said to be palindrome when it remains same to the original number when it is reversed.
    for example : 51415 after reversing it remains same as 51415, so it's a palindrome number

---
+++ Palindrome.java
@@ -0,0 +1,27 @@
+import java.util.*;
+public class Palindrome {
+    public static boolean palindromeNumber(int n){
+        int x=n;
+        int rev=0;
+        while(x!=0){
+            int rem=x%10;
+            rev=rev*10+rem;
+            x=x/10;
+        }
+        if(n==rev){
+            return true;
+        }else{
+            return false;
+        }
+        /*if(x!=rev || x<0){   //leetcode condition (bcs for -121 the output is 121- which is not palindrome)
+            return false;
+        }else{
+            return true;
+        }*/
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println(palindromeNumber(n));
+    }
+}
