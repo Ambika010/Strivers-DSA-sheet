07d2cff95c800404b668e19ed4cf0ec4d4b4bffa
Ambika Kumari <145006866+Ambika010@users.noreply.github.com>
2023-11-08 22:49:28 +0530

    Add files via upload
    GCD (greatest common divisor) i.e gcd of two number is equal to the greatest number that completely divides both the number
    Input: num1 = 4, num2 = 8
    Output: 4
    Explanation: Since 4 is the greatest number which divides both num1 and num2.

---
+++ GCD_or_HCF.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class GCD_or_HCF {
+    public static int calcGCD(int n, int m){
+        int gcd=1;
+        for(int i=1;i<=n && i<=m;i++){
+            if(n%i==0 && m%i==0){
+                gcd=i;
+            }
+        }
+        return gcd;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        System.out.println(calcGCD(n,m));
+    }
+}
