c0df8fa35c11a9c70738e3020a01d6eda460d11e
Ambika Kumari <145006866+Ambika010@users.noreply.github.com>
2023-11-09 21:27:20 +0530

    Add files via upload
    This program checks the whether a number is prime or not
    A number is said to be prime if and only if it is divisible by 1 and the number itself (i.e the number should not have any other divisor than these two)

---
+++ Check_for_Prime.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class Check_for_Prime {
+    public static String isPrime(int num) {
+        boolean prime = true;
+        if(num==1){
+          prime=false;
+        }else{
+          for (int i = 2; i < num; i++) {
+                if (num % i == 0) {
+                    prime = false;
+                    break;
+                }
+            }
+        }
+        return prime ? "YES" : "NO";
+    }
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        System.out.println(isPrime(n));
+    }
+}
