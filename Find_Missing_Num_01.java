f52bb2f46d39e577a88f7d9aae278ba14c591090
Ambika Kumari <145006866+Ambika010@users.noreply.github.com>
2024-02-01 21:59:15 +0530

    Update Find_Missing_Num_01.java

--- Find_Missing_Num_01.java
+++ Find_Missing_Num_01.java
@@ -9,6 +9,8 @@ Explanation: n = 3 since there are 3 numbers, so all numbers are in the range [0
 public class Find_Missing_Num_01 {
     public static int missingNumber(int[] nums){
         int n=nums.length;
+        /* why is the size of hash array n+1 ?? 
+        For example, if N is 3, you want indices 0, 1, 2, and 3 to represent frequencies for numbers 0, 1, 2, and 3. Hence, you create an array of size 4 (N + 1) to cover the range from 0 to N. */
         int hash[] = new int[n+1]; //This array will be used to store the frequencies of numbers in the array i.e. how many times the number is appearing in the array
         for(int i=0;i<n;i++){
             hash[nums[i]]++; //if input array = {3,0,1} then hash array = {1,1,0,1} [initially, hash array = {0,0,0,0}]
